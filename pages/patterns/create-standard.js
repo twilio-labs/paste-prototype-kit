ceacbac20d92bd99efcbc9401ecac8b8620c1a6b
cdelaomartinez <cdelaomartinez@twilio.com>
2021-02-01 13:19:09 -0800

    chore: added link to paste create pattern doc

--- pages/patterns/create-standard.js
+++ pages/patterns/create-standard.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom';
 import { Theme } from '@twilio-paste/core/theme';
 import { Box } from '@twilio-paste/core/box';
 import { Button } from '@twilio-paste/core/button';
+import { Alert } from '@twilio-paste/core/alert';
+import { Text } from '@twilio-paste/core/text';
+import { Anchor } from '@twilio-paste/core/anchor';
 import { Heading } from '@twilio-paste/core/heading';
 import { Paragraph } from '@twilio-paste/core/paragraph';
 import { Stack } from '@twilio-paste/core/stack';
 import { useToaster, Toaster } from '@twilio-paste/core/toast';
 
+import { Code } from '../../components/site/Code';
 import { CreateServiceModal } from '../../components/site/patterns/Create Patterns/CreateServiceModal';
 import { ServicesTable } from '../../components/site/patterns/Create Patterns/ServicesTable';
 import { serviceData } from '../../components/site/patterns/Create Patterns/data';
@@ -43,9 +47,20 @@ export default function Create() {
   };
 
   return (
-    <Theme.Provider theme="default">
-      {/* This renders in a portal so it can be anywhere in our JSX */}
-      <Toaster {...toaster} />
+    <>
+      <Box marginBottom="space100">
+        <Alert variant="neutral">
+          <Text as="span">
+            <strong>Hello! </strong>To edit this page go to the &quot;pages&quot; folder and click the
+            &quot;create-standard.js&quot; file. Check out the{' '}
+            <Anchor href="https://paste.twilio.design/patterns/create/#standard" showExternal>
+              Paste Create Pattern documentation
+            </Anchor>{' '}
+            for more information. To get rid of this alert box, remove the <Code>{'<Box></Box>'}</Code> component from
+            the bottom of the &quot;create-standard.js&quot; file.
+          </Text>
+        </Alert>
+      </Box>
       <Box marginBottom="space60" marginTop={['space10', 'space110']} marginLeft="space60" marginRight="space60">
         <Stack orientation="vertical" spacing="space70">
           <Box alignItems="center" display="flex">
@@ -69,6 +84,8 @@ export default function Create() {
           <ServicesTable services={services} />
         </Stack>
       </Box>
-    </Theme.Provider>
+      <Toaster {...toaster} />
+
+    </>
   );
 }
