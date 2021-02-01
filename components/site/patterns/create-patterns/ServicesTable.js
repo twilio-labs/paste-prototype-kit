357d99234b129ee4a01f543f19a9738574f18df8
cdelaomartinez <cdelaomartinez@twilio.com>
2021-02-01 13:19:09 -0800

    chore: added link to create-pattern refactored create-pattern for usability
    chore: refactored create pattern for usability
    
    chore: renamed create patterns forlder to create-pattern and changed the port number of the test script

--- components/site/patterns/create-patterns/ServicesTable.js
+++ components/site/patterns/create-patterns/ServicesTable.js
@@ -4,7 +4,7 @@ import { Anchor } from '@twilio-paste/core/anchor';
 import { Table, THead, TBody, Tr, Th, Td } from '@twilio-paste/core/table';
 import PropTypes from 'prop-types';
 
-export const ServicesTable = ({ services }) => {
+export const ServicesTable = ({ services, col1, col2, col3 }) => {
   // Empty state handling
   if (services.length === 0) {
     return <Text as="div">No services to display</Text>;
@@ -14,25 +14,25 @@ export const ServicesTable = ({ services }) => {
     <Table tableLayout="fixed" data-cy="create-services-table">
       <THead>
         <Tr>
-          <Th>Friendly Name</Th>
-          <Th>SID</Th>
-          <Th>Date Created</Th>
+          <Th>{col1}</Th>
+          <Th>{col2}</Th>
+          <Th>{col3}</Th>
         </Tr>
       </THead>
       <TBody>
-        {services.map(({ name, sid, created }, index) => {
+        {services.map((item, index) => {
           return (
-            <Tr key={index} data-cy={`${name}-row`}>
+            <Tr key={index} data-cy={`${item[col1]}-row`}>
               <Td>
-                <Anchor href="#">{name}</Anchor>
+                <Anchor href="#">{item[col1]}</Anchor>
               </Td>
               <Td>
                 <Text as="span" fontFamily="fontFamilyCode">
-                  {sid}
+                  {item[col2]}
                 </Text>
               </Td>
               <Td>
-                <Text as="span">{created}</Text>
+                <Text as="span">{item[col3]}</Text>
               </Td>
             </Tr>
           );
@@ -45,8 +45,14 @@ export const ServicesTable = ({ services }) => {
 // These ensure that the props passed from the parent component are present and/or correctly typed
 ServicesTable.propTypes = {
   services: PropTypes.arrayOf(PropTypes.object),
+  col1: PropTypes.string,
+  col2: PropTypes.string,
+  col3: PropTypes.string,
 };
 
 ServicesTable.defaultProps = {
   services: [],
+  col1: '',
+  col2: '',
+  col3: '',
 };
--- pages/patterns/create-standard.js
+++ pages/patterns/create-standard.js
@@ -1,20 +1,54 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import { Theme } from '@twilio-paste/core/theme';
 import { Box } from '@twilio-paste/core/box';
 import { Button } from '@twilio-paste/core/button';
+import { Alert } from '@twilio-paste/core/alert';
+import { Text } from '@twilio-paste/core/text';
+import { Anchor } from '@twilio-paste/core/anchor';
 import { Heading } from '@twilio-paste/core/heading';
 import { Paragraph } from '@twilio-paste/core/paragraph';
 import { Stack } from '@twilio-paste/core/stack';
 import { useToaster, Toaster } from '@twilio-paste/core/toast';
 
-import { CreateServiceModal } from '../../components/site/patterns/Create Patterns/CreateServiceModal';
-import { ServicesTable } from '../../components/site/patterns/Create Patterns/ServicesTable';
-import { serviceData } from '../../components/site/patterns/Create Patterns/data';
+import { Code } from '../../components/site/Code';
+import { CreateServiceModal } from '../../components/site/patterns/create-patterns/CreateServiceModal';
+import { ServicesTable } from '../../components/site/patterns/create-patterns/ServicesTable';
+
+/*
+ * Comment the other and uncomment this to view/edit the empty state
+ * export const serviceData = [];
+ */
 
 export default function Create() {
+  // column names
+  const NameForColumn1 = 'Service Name';
+  const NameForColumn2 = 'SID';
+  const NameForColumn3 = 'Created';
+  // example sample data
+  const exampleServiceData = [
+    {
+      [NameForColumn1]: 'Montanõ - Valencia',
+      [NameForColumn2]: 'SM0yc4mxi6cn4z13bte7qmflc2drc85mlp',
+      [NameForColumn3]: 'Tue Jan 12 2021 15:42:05 GMT-0700 (Mountain Standard Time)',
+    },
+    {
+      [NameForColumn1]: 'Mante Inc',
+      [NameForColumn2]: 'SMl29llgoihx286uhxfb0yc5n0sg391x5n',
+      [NameForColumn3]: 'Tue Jan 12 2021 15:42:05 GMT-0700 (Mountain Standard Time)',
+    },
+    {
+      [NameForColumn1]: 'Mante Inc',
+      [NameForColumn2]: 'SMxarke3v30fv17hauqn86a7nhgm3b5d87',
+      [NameForColumn3]: 'Tue Jan 12 2021 15:42:05 GMT-0700 (Mountain Standard Time)',
+    },
+    {
+      [NameForColumn1]: 'Kihn - Ondricka',
+      [NameForColumn2]: 'US0yc4mxi6cn4z13bte7qmflc2drc85mlp',
+      [NameForColumn3]: 'Tue Jan 12 2021 15:42:05 GMT-0700 (Mountain Standard Time)',
+    },
+  ];
+
   // Table state
-  const [services, setServices] = React.useState(serviceData);
+  const [services, setServices] = React.useState(exampleServiceData);
   // Modal state
   const [isModalOpen, setModalOpen] = React.useState(false);
   const openModal = () => setModalOpen(true);
@@ -26,9 +60,9 @@ export default function Create() {
     // Update our table state
     setServices([
       {
-        name: serviceName,
-        sid: serviceSID,
-        created: new Date().toString(),
+        [NameForColumn1]: serviceName,
+        [NameForColumn2]: serviceSID,
+        [NameForColumn3]: new Date().toString(),
       },
       ...services,
     ]);
@@ -43,32 +77,49 @@ export default function Create() {
   };
 
   return (
-    <Theme.Provider theme="default">
-      {/* This renders in a portal so it can be anywhere in our JSX */}
-      <Toaster {...toaster} />
-      <Box marginBottom="space60" marginTop={['space10', 'space110']} marginLeft="space60" marginRight="space60">
-        <Stack orientation="vertical" spacing="space70">
-          <Box alignItems="center" display="flex">
-            <Heading as="h1" variant="heading10" marginBottom="space0">
-              Services
-            </Heading>
-            <Box marginLeft="auto" data-cy="create-new-service">
-              <Button variant="primary" onClick={openModal}>
-                Create new service
-              </Button>
-              {/* This renders the modal, depending isModalOpen */}
-              <CreateServiceModal isModalOpen={isModalOpen} handleClose={closeModal} onSubmit={handleFormSubmit} />
-            </Box>
-          </Box>
-          <Paragraph>
-            Services enable you to organize and identify your conversations by use case, or manage them in multiple
-            environments (e.g. dev, stage, prod). This information held within a service is siloed, protecting both your
-            recipients&apos; data.
-          </Paragraph>
-          {/* This renders the services table */}
-          <ServicesTable services={services} />
-        </Stack>
+    <>
+      <Box marginBottom="space100">
+        <Alert variant="neutral">
+          <Text as="span">
+            <strong>Hello! </strong>To edit this page go to the &quot;pages&quot; folder and click the
+            &quot;create-standard.js&quot; file. Check out the{' '}
+            <Anchor href="https://paste.twilio.design/patterns/create/#standard" showExternal>
+              Paste Create Pattern documentation
+            </Anchor>{' '}
+            for more information. To get rid of this alert box, remove the first <Code>{'<Box></Box>'}</Code> component,
+            and its contents, from the &quot;create-standard.js&quot; file.
+          </Text>
+        </Alert>
       </Box>
-    </Theme.Provider>
+      <Stack orientation="vertical" spacing="space70">
+        <Box alignItems="center" display="flex">
+          <Heading as="h1" variant="heading10" marginBottom="space0">
+            Services
+          </Heading>
+          <Box marginLeft="auto" data-cy="create-new-service">
+            <Button variant="primary" onClick={openModal}>
+              Create new service
+            </Button>
+            {/* This renders the modal, depending isModalOpen */}
+            <CreateServiceModal
+              isModalOpen={isModalOpen}
+              handleClose={closeModal}
+              onSubmit={handleFormSubmit}
+              col1={NameForColumn1}
+              col2={NameForColumn2}
+            />
+          </Box>
+        </Box>
+        <Paragraph>
+          Services enable you to organize and identify your conversations by use case, or manage them in multiple
+          environments (e.g. dev, stage, prod). This information held within a service is siloed, protecting both your
+          recipients&apos; data.
+        </Paragraph>
+        {/* This renders the services table */}
+        <ServicesTable services={services} col1={NameForColumn1} col2={NameForColumn2} col3={NameForColumn3} />
+      </Stack>
+
+      <Toaster {...toaster} />
+    </>
   );
 }
