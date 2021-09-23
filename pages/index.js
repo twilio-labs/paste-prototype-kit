ff4139c744ee5e0d76d0ad53a34220ff22afd612
Nora Krantz <nkrantz@twilio.com>
2021-09-23 12:57:11 -0600

    chore(prototype-kit): pop changes

--- pages/index.js
+++ pages/index.js
@@ -168,23 +168,65 @@ export default function Home() {
           NextJS
         </PrototypeAnchor>{' '}
         app with Paste preloaded. It has everything you will need to create a fully functioning web based prototype
-        using Paste components.
+        using Paste components
       </Paragraph>
-      <Paragraph>
-        <strong>Edit your prototype </strong>right in your browser using CodeSandbox
-      </Paragraph>
-      <Anchor display="inline-flex" href="https://githubbox.com/twilio-labs/paste-prototype-kit">
-        <Box
-          marginBottom="space80"
-          as="img"
-          src="https://codesandbox.io/static/img/play-codesandbox.svg"
-          alt="Edit in Codesandbox"
-        />
-      </Anchor>
-      <Paragraph>
-        <strong>Share your prototype quickly</strong> by deploying it using Vercel or Netlify:
-      </Paragraph>
+      <Grid gutter="space40" marginBottom="space70" equalColumnHeights>
+        <Column>
+          <Card>
+            <Heading as="h3" variant="heading30">
+              Creating pages
+            </Heading>
+            <Paragraph>
+              Create pages in the <Code>pages</Code> directory. You can copy the <Code>template-page.js</Code> file and
+              rename it as <Code>my-new-page.js</Code> to get you going. The name of the file in the <Code>pages</Code>{' '}
+              directory will result in a route of the same name. e.g. <Code>my-new-page.js</Code> =&gt;{' '}
+              <Code>/my-new-page</Code>.
+            </Paragraph>
+          </Card>
+        </Column>
+        <Column>
+          <Card>
+            <Heading as="h3" variant="heading30">
+              Adding to the side nav
+            </Heading>
+            <Paragraph>
+              To add a link to a new page in the side bar, add the new page&apos;s file name to the pages.json file in
+              the <Code>pages</Code> directory. Make sure it is in quotes and followed by <Code>: []</Code>. For
+              example:
+            </Paragraph>
+            <Paragraph>
+              <Code>&quot;my-new-page&quot;: []</Code>
+            </Paragraph>
+            <Paragraph>
+              <strong>Note:</strong> if you are adding to the middle of the list, end the new line with a comma. If it
+              is the last list item, do not end the line with a comma.
+            </Paragraph>
+          </Card>
+        </Column>
+        <Column>
+          <Card>
+            <Heading as="h3" variant="heading30">
+              Linking between pages
+            </Heading>
+            <Paragraph>
+              <strong>Internal linking: </strong>Use the <Code>&lt;PrototypeAnchor /&gt;</Code> not the Paste Anchor. It
+              is a wrapper around the Paste Anchor suitable for NextJS websites. It looks and behaves exactly like a{' '}
+              <PrototypeAnchor href="https://paste.twilio.design/components/anchor" showExternal>
+                Paste Anchor
+              </PrototypeAnchor>
+              .
+            </Paragraph>
+          </Card>
+        </Column>
+      </Grid>
+      <Heading as="h2" variant="heading20">
+        Ways to get started
+      </Heading>
+      <Paragraph>The prototyping kit can be quickly deployed to the following platforms:</Paragraph>
       <Stack orientation="horizontal" spacing="space40">
+        <Anchor display="inline-flex" href="https://githubbox.com/twilio-labs/paste-prototype-kit">
+          <Box as="img" src="https://codesandbox.io/static/img/play-codesandbox.svg" alt="Edit in Codesandbox" />
+        </Anchor>
         <Anchor
           display="inline-flex"
           href="https://vercel.com/import/project?template=https://github.com/twilio-labs/paste-prototype-kit/tree/main"
