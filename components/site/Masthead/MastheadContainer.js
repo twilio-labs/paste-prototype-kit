e5a8935bdb5ee35e010f981933c5cc944671f5ba
PoojMenon <72031986+PoojMenon@users.noreply.github.com>
2021-10-14 18:08:07 +0100

    replaced double quotes with singles

--- components/site/Masthead/MastheadContainer.js
+++ components/site/Masthead/MastheadContainer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Box } from "@twilio-paste/core/box";
 
 export const MastheadContainer = ({ children }) => {
